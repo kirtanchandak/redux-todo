004972c1fa7404212f59f56e4582bceb32c4c085
kirtanchandak <themarketingkirtan@gmail.com>
2023-09-12 12:15:47 +0530

    delete todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { deletetodo } from "../store/features/todo/todoSlice";
 
 function Todos() {
   const todos = useSelector((state) => state.todo.todos);
   console.log(todos);
+
+  const dispatch = useDispatch();
   return (
     <>
       {todos.map((todo) => (
-        <div>
+        <div
+          key={todo.id}
+          style={{ display: "flex", gap: "25px", marginTop: "30px" }}
+        >
           <h4>{todo.title}</h4>
+          <button
+            style={{ marginTop: "0px" }}
+            onClick={() => dispatch(deletetodo(todo.id))}
+          >
+            delete
+          </button>
         </div>
       ))}
     </>
